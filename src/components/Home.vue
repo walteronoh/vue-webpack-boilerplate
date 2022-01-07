80eefb6de315296c6bdb3e4245d09d503027c597
walteronoh <walterkiprono81@gmail.com>
2022-01-07 22:15:08 +0300

    chore: add html webpack plugin and routing

---
+++ src/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Simple vue app using webpack</h1>
+    </div>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "[name].js"
     },
     module: {
         rules: [
-            { exclude: /node_modules/, test: /\.js$/, use: "babel-loader" },
-            { test: /\.vue$/, use: "vue-loader" }
-        ]
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.vue$/, loader: "vue-loader" }
+        ],
     },
-    devtool: devMode ? "eval" : "source-map",
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.esm.js"
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [new VueLoaderPlugin(), new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html"
+    })],
+    devtool: devMode ? "eval-source-map" : "source-map",
     mode: devMode ? "development" : "production"
 };
